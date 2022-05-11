755e98408c77b68463d9902e80d901742a3aa164
Erika Brownlee <eebrownl@gmail.com>
2022-05-11 11:16:12 -0600

    Scrapped original code and started over. Wrote functions to control display.

--- script.js
+++ script.js
@@ -3,20 +3,41 @@ const operatorButtons = document.querySelectorAll('.operator');
 const allClearButton = document.querySelector('.all-clear');
 const clearButton = document.querySelector('.clear');
 const equalsButton = document.querySelector('.equals');
-const firstOperand = document.querySelector('.first-operand');
-const secondOperand = document.querySelector('.second-operand');
+const display = document.querySelector('.display');
 
-numberButtons.forEach(button => {
-    button.addEventListener('click', () => console.log('working'))
+for(let i = 0; i < numberButtons.length; i++) {
+    numberButtons[i].addEventListener('click', e => {
+        const key = e.target;
+        const keyValue = key.textContent;
+        const displayValue = display.textContent;
+
+        if (parseInt(displayValue) === 0) {
+            display.textContent = keyValue;
+        } else {
+            display.textContent = displayValue + keyValue
+        }
+    }) 
+}
+
+for(let i = 0; i < operatorButtons.length; i++) {
+    operatorButtons[i].addEventListener('click', e => {
+        const key = e.target;
+        const keyValue = key.textContent
+    }) 
+}
+
+allClearButton.addEventListener('click', e => {
+    const allClearKey = e.target;
+    display.textContent = '0';
 })
 
-operatorButtons.forEach(button => {
-    button.addEventListener('click', () => console.log('working'))
+clearButton.addEventListener('click', e => {
+    const clearKey = e.target;
+    const displayValue = display.textContent;
+    let newDisplayValue = displayValue.slice(0, -1);
+    display.textContent = newDisplayValue;
 })
-
-allClearButton.addEventListener('click', test);
-clearButton.addEventListener('click', test);
-equalsButton.addEventListener('click', test);
+equalsButton.addEventListener('click', test)
 
 function test() {
     console.log('working')
