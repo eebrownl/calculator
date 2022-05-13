1ded6fb1b742c0e587ac961d307ec55b5718e9eb
Erika Brownlee <eebrownl@gmail.com>
2022-05-13 10:23:46 -0600

    Scrapped 2nd try and got display wired on 3rd try

--- script.js
+++ script.js
@@ -1,44 +1,28 @@
-const numberButtons = document.querySelectorAll('.number');
-const operatorButtons = document.querySelectorAll('.operator');
-const allClearButton = document.querySelector('.all-clear');
-const clearButton = document.querySelector('.clear');
-const equalsButton = document.querySelector('.equals');
-const display = document.querySelector('.display');
+const container = document.querySelector('.container')
+const keys = container.querySelector('.keys')
+const display = container.querySelector('.display')
 
-for(let i = 0; i < numberButtons.length; i++) {
-    numberButtons[i].addEventListener('click', e => {
-        const key = e.target;
-        const keyValue = key.textContent;
-        const displayValue = display.textContent;
+keys.addEventListener('click', e => {
+    const key = e.target
+    const keyValue = key.textContent
+    const displayValue = display.textContent
+    const action = key.dataset.action
+    let previousKeyType = keys.dataset.previousKeyType
 
-        if (parseInt(displayValue) === 0) {
-            display.textContent = keyValue;
+    if (action ==='add' ||
+        action ==='subtract' ||
+        action ==='multiply' ||
+        action ==='divide') {
+            keys.dataset.previousKeyType = 'operator';
+        }
+
+    if (!action) {
+        keys.dataset.previousKeyType = 'number';
+
+        if (displayValue ==='0' || previousKeyType ==='operator') {
+            display.textContent = keyValue
         } else {
             display.textContent = displayValue + keyValue
         }
-    }) 
-}
-
-for(let i = 0; i < operatorButtons.length; i++) {
-    operatorButtons[i].addEventListener('click', e => {
-        const key = e.target;
-        const keyValue = key.textContent
-    }) 
-}
-
-allClearButton.addEventListener('click', e => {
-    const allClearKey = e.target;
-    display.textContent = '0';
+    }
 })
-
-clearButton.addEventListener('click', e => {
-    const clearKey = e.target;
-    const displayValue = display.textContent;
-    let newDisplayValue = displayValue.slice(0, -1);
-    display.textContent = newDisplayValue;
-})
-equalsButton.addEventListener('click', test)
-
-function test() {
-    console.log('working')
-}
